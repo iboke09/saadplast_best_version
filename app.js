20eae6330ae2bed091b0e2f3d3f573417a469d92
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-10-08 22:30:01 +0300

    update every thing

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-  new Splide('#testimonial-slider', {
-    type: 'loop',
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#testimonial-slider", {
+    type: "loop",
     perPage: 2,
     perMove: 1,
-    focus: 'left',
+    focus: "left",
     autoplay: true,
     interval: 7000,
     pauseOnHover: true,
-    gap: '0px', // المسافة بين البطاقات
+    gap: "0px",
     pagination: false,
-    arrows: false, // إخفاء أزرار التبديل
+    arrows: false,
   }).mount();
+});
+
+let burgerMenu = document.getElementById("burger-menu");
+let navMenu = document.getElementById("navMenu");
+let clicked = document.getElementById("clicked");
+burgerMenu.addEventListener("click", () => {
+  clicked.style.cssText = "display: block";
+  burgerMenu.style.cssText = "display: none";
+  navMenu.style.cssText = "display: block";
+});
+clicked.addEventListener("click", () => {
+  clicked.style.cssText = "display: none";
+  burgerMenu.style.cssText = "display: block";
+  navMenu.style.cssText = "display: none";
 });
 
